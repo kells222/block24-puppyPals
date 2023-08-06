0a08f5dad3a71664402216fb16d698c6b82fa428
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-08-05 17:59:02 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-
+import { useState } from 'react'
 import './App.css'
+import { puppyList } from './data'
 
 function App() {
   
+  const [puppies, setPuppies] = useState(puppyList)
+  // console.log(puppies)
+  console.log("puppyList:", puppyList)
 
   return (
     <>
-      <div>
-
+      <div className='App'>
+      {
+        puppies.map((puppy)=>{
+          return <p>{puppy.name}</p>
+        })
+      }
       </div>
       
     </>
