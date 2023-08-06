a54eadb0f3e1b566ee23d433112c9874ea704911
Kelly Buckley <princesskells@Kellys-MacBook-Air.local>
2023-08-05 19:04:10 -0700

    finishing up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import { puppyList } from './data'
+import './App.css'
 
 function App() {
-  
   const [puppies, setPuppies] = useState(puppyList)
   // console.log(puppies)
   // console.log("puppyList:", puppyList)
-
   const [featPupId, setFeatPupId] = useState(null)
   // console.log(featPupId)
 
@@ -41,12 +40,3 @@ function App() {
 
 export default App
 
-// {featPupId && (
-//   <div>
-//     <h2>{featuredPup.name}</h2>
-//     <ul>
-//       <li>Age: {featuredPup.age}</li>
-//       <li>Email: {featuredPup.email}</li>
-//     </ul>
-//   </div>
-// )}
